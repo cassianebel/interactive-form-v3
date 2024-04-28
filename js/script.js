2f75ccfa033c36182ed562c7349b9fe68cf8284c
Cassia Nebel <162936759+cassianebel@users.noreply.github.com>
2024-04-27 19:56:09 -0500

    disable color options, then enable options based on t-shirt theme selection

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ nameInput.focus();
 
 /**
  * Hide the 'other-job-role' input onload,
- * display it based on user input
+ * display it based on user input.
  */
 const otherJobInput = document.getElementById('other-job-role');
 otherJobInput.style.display = 'none';
@@ -24,5 +24,29 @@ jobTitleSelect.addEventListener('change', () => {
     otherJobInput.style.display = 'block';
   } else {
     otherJobInput.style.display = 'none';
+  }
+});
+
+
+/**
+ * Disable the t-shirt color select onload.
+ */
+const colorSelect = document.getElementById('color');
+colorSelect.disabled = true;
+
+/**
+ * Display available color options based on t-shirt theme selection.
+ */
+const designSelect = document.getElementById('design');
+designSelect.addEventListener('change', () => {
+  colorSelect.disabled = false;
+  const colorOptions = colorSelect.querySelectorAll('option');
+  for (let i = 0; i < colorOptions.length; i++) {
+    if (colorOptions[i].getAttribute('data-theme') !== designSelect.value) {
+      colorOptions[i].hidden = true;
+    } else {
+      colorOptions[i].hidden = false;
+      colorOptions[i].selected = true;
+    }
   }
 });
