69bbf2932d31b2b556f0f3300c0dea5a6f73838a
Cassia Nebel <162936759+cassianebel@users.noreply.github.com>
2024-04-27 19:23:46 -0500

    initiate focus on name input and hide/display other job input based on job title select

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-console.log('test');
+/**
+ * Cassia Nebel's
+ * Interactive Form Project
+ */
+
+
+/**
+ * initiate the focus on the Name input
+ */
+const nameInput = document.getElementById('name');
+nameInput.focus();
+
+
+/**
+ * Hide the 'other-job-role' input onload,
+ * display it based on user input
+ */
+const otherJobInput = document.getElementById('other-job-role');
+otherJobInput.style.display = 'none';
+
+const jobTitleSelect = document.getElementById('title');
+jobTitleSelect.addEventListener('change', () => {
+  if (jobTitleSelect.value === 'other') {
+    otherJobInput.style.display = 'block';
+  } else {
+    otherJobInput.style.display = 'none';
+  }
+});
